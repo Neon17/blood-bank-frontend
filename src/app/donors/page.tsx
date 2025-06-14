942c3b01819f3cf7cb98abb9a5030dcdc8b9766c
Neon17 <neon.neupane.9@gmail.com>
2025-06-14 09:36:09 +0545

    feat: sample donor and requests of blood UI made in frontend

---
+++ src/app/donors/page.tsx
@@ -0,0 +1,47 @@
+export default async function Donors() {
+
+    return (
+        <main className="flex flex-col min-h-screen w-full items-center justify-center text-black requests-page">
+            <h1 className="text-4xl font-bold">Requests</h1>
+
+            <div className="flex flex-wrap justify-center gap-4 mt-6">
+                    <div
+                        className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm"
+                    >
+                        <a href="#">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                                "No Donor API made"
+                            </h5>
+                        </a>
+                        <p className="mb-3 font-normal text-gray-700">
+                            This is a sample donor description.
+                        </p>
+                        <a
+                            href="#"
+                            className="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                        >
+                            Read more
+                            <svg
+                                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 14 10"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                                />
+                            </svg>
+                        </a>
+                    </div>
+                
+            </div>
+
+
+        </main>
+    )
+}
--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -1,11 +1,23 @@
 "use server"
 
-import { redirect } from "next/navigation";
 import api from "./axios";
 
 export async function profile () {
     try {
         const response = await api.get('/profile');
+        const data = await response.data;
+        return data;
+    } catch (error: any | { message: string }) {
+        return {
+            status: 'error',
+            message: error.message
+        }
+    }
+}
+
+export async function BloodRequests() {
+    try {
+        const response = await api.get('/blood/requests');
         const data = await response.data;
         return data;
     } catch (error: any | { message: string }) {
